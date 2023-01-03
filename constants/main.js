e5b4629089541d2ee7bbad364bdfb80674f0667d
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2023-01-03 09:40:57 +0700

    add env

--- constants/main.js
+++ constants/main.js
@@ -1,6 +1,6 @@
 import { Provider, Wallet } from "zksync-web3";
 
-const PRIVATE_KEY = "8ed772c06d5241f5b82ee5b950dabc21c11916d4fdcd6053d3b0ff864c00c9c0";
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 export const provider = new Provider("https://zksync2-testnet.zksync.dev");
 export const deployAccount = new Wallet(PRIVATE_KEY).connect(provider);
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-}
+    reactStrictMode: false,
+    env: {
+        PRIVATE_KEY: process.env.PRIVATE_KEY,
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
